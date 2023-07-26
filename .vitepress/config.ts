670719ab8fd0ef5fb23e4d7883e7f889466fc06a
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-07-26 18:28:27 +0200

    add meta

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,11 +4,34 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Rollkit",
   description: "The open modular framework for sovereign rollups.",
+  
+  head: [
+    // ['link', { rel: 'icon', href: '/favicon-dark.svg', type: 'image/svg+xml', media: '(prefers-color-scheme: dark)' }],
+    // ['link', { rel: 'icon', href: '/favicon-dark.png', type: 'image/png', media: '(prefers-color-scheme: dark)' }],
+    ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
+    ['link', { rel: 'icon', href: '/favicon.png', type: 'image/png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#fff' }],
+    ['meta', { name: 'theme-color', content: '#fff' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }],
+    ['meta', { property: 'og:title', content: 'Rollkit' }],
+    ['meta', { property: 'og:description', content: 'The open modular framework for sovereign rollups.' }],
+    ['meta', { property: 'description', content: 'The open modular framework for sovereign rollups.' }],
+    ['meta', { httpEquiv: 'Content-Language', content: 'en' }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:image', content: '/Rollkit-og.png' }],
+    ['meta', { name: 'twitter:site:domain', content: 'rollkit.dev' }],
+    ['meta', { name: 'twitter:url', content: 'https://rollkit.dev' }],
+    ['meta', { name: 'og:image', content: '/Rollkit-og.png' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'Rollkit' }],
+  ],
+
+  appearance: 'dark',
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/rollup-starter' }
     ],
 
     sidebar: [
@@ -23,8 +46,15 @@ export default defineConfig({
       }
     ],
 
+    logo: {
+      alt: 'Rollkit Logo',
+      light: '/logo.svg',
+      dark: '/logo-dark.svg'
+    },
+
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/rollkit/docs' },
+      { icon: 'twitter', link: 'https://twitter.com/RollkitDev' }
     ]
   }
 })
