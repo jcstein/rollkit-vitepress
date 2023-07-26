0ca5eaaa1861d3dcbb26dfac58fde890aa7057fa
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-07-26 15:48:12 +0200

    init

---
+++ .vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Rollkit",
+  description: "The open modular framework for sovereign rollups.",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Rollup Starter', link: '/rollup-starter'},
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ README.md
@@ -0,0 +1,15 @@
+# Rollkit Docs
+
+## Built with VitePress
+
+Install dependencies:
+
+```bash
+pnpm i
+```
+
+Build the docs site locally:
+
+```bash
+pnpm run docs:dev
+```
